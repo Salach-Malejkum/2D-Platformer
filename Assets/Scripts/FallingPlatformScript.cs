a2528deaaab543b1ae0e684ba09327e9078101b5
Mateusz Salach <mateuszsalach00@gmail.com>
2022-06-14 20:38:04 +0200

    Add falling platform functionality

--- Assets/Scripts/FallingPlatformScript.cs
+++ Assets/Scripts/FallingPlatformScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FallingPlatformScript : MonoBehaviour
 {
+    public float fallDelay = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,19 @@ public class FallingPlatformScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+    
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        GetComponent<Rigidbody2D>().isKinematic = false;
     }
 }
